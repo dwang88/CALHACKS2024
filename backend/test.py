0def240bd42dfb07995f85bf28c2a5f19d10724f
Annie Yin <anniesy2@uci.edu>
2024-06-22 14:20:46 -0700

    add_teacher adds teachers to database(new)

--- backend/test.py
+++ backend/test.py
@@ -4,16 +4,39 @@ import pymongo
 
 uri = "mongodb+srv://anniesy2:Calhacks2024@teachers.6rnbrpj.mongodb.net/?retryWrites=true&w=majority&appName=Teachers"
 
-client = MongoClient(uri, server_api=ServerApi('1'))
 
-try:
-    mydb = client["testing"]
-    two = mydb["here"]
-    new = two.insert_one({"name": "Alice", "age": 30})
-    # print("done")
-except Exception as e:
-    print(e)
+def test():
+    client = MongoClient(uri, server_api=ServerApi('1'))
 
+    try:
+        mydb = client["testing"]
+        two = mydb["test3"]
+        three = two["test4"]
+        new = three.insert_one({"hi": "goodbye"})
+        # new = two.insert_one({})
+        # new = two.insert_one({"name": "Alice", "age": 30})
+        # print("done")
+    except Exception as e:
+        print(e)
+
+
+def wrap():
+    client = MongoClient(uri, server_api=ServerApi('1'))
+    try:
+        db = client["StudentInformationTest"]
+        add_teacher("Dog", "Dogs", "Dogs2", db)
+    except Exception as e:
+        print(e)
+    finally:
+        client.close()
+
+
+def add_teacher(teacher, username, pwd, db):
+    new_teacher = db[teacher]
+    insert_new = {"username": username, "pwd": pwd}
+    new_teacher.insert_one(insert_new)
+    
+wrap()
 
 """
 {
