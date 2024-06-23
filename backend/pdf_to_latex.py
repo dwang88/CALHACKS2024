11c8e862fec9d232f8c7d04a7a6b017fb5e5a6fd
dwang88 <davidwang1089@gmail.com>
2024-06-22 23:20:18 -0700

    pdf to img to latex llm

--- backend/pdf_to_latex.py
+++ backend/pdf_to_latex.py
@@ -3,6 +3,10 @@ import fitz
 from PIL import Image
 import base64
 import openai
+import boto3
+import json
+from pypdf import PdfReader 
+
 
 from dotenv import load_dotenv
 
@@ -26,7 +30,7 @@ def pdf_to_images(pdf_path, output_folder):
 
     print(f"PDF has been successfully converted to images in the folder: {output_folder}")
 
-pdf_path = "latex.pdf"  
+pdf_path = "math.pdf"  
 output_folder = "output_images"
 pdf_to_images(pdf_path, output_folder)
 
@@ -45,7 +49,7 @@ for image_name in os.listdir(output_folder):
                 {
                     "role": "user",
                     "content": [
-                        {"type": "text", "text": "What’s in this image?"},
+                        {"type": "text", "text": "Transcribe the image (which might contain latex) to text."},
                         {
                             "type": "image_url",
                             "image_url": {
@@ -58,5 +62,45 @@ for image_name in os.listdir(output_folder):
             max_tokens=300,
         )
 
+        print("RAW LATEX OUTPUT:")
         print(f"Response for {image_name}:")
-        print(response.choices[0])
+        print(response.choices[0]) 
+        
+        # new part
+        user_prompt = response.choices[0].message.content.strip()
+
+        print("")
+        print(user_prompt)
+
+        bedrock = boto3.client(service_name="bedrock-runtime", region_name='us-east-1')
+
+        modelId = "anthropic.claude-3-haiku-20240307-v1:0"
+
+        accept = "application/json"
+        contentType = "application/json"
+        system_prompt = "You are a helpful assistant. Do not give the user the answer directly, but guide them towards finding the answer. format your answer in latex"
+
+        response = bedrock.invoke_model(
+            modelId=modelId,
+            contentType=contentType,
+            accept=accept,
+            body=json.dumps(
+                {
+                    "anthropic_version": "bedrock-2023-05-31",
+                    "max_tokens": 1024,
+                    "messages": [
+                        {
+                            "role": "user",
+                            "content": [{"type": "text", "text": system_prompt + " " + user_prompt}]
+                        }
+                    ]
+                }
+            )
+        )
+
+        result = json.loads(response.get("body").read().decode('utf-8'))
+        output_list = result.get("content", [])
+
+        for output in output_list:
+            print("SOLUTION OUTPUT:")
+            print(output["text"])
