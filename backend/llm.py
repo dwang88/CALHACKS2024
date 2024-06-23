c7f2d4b40787bd2af72fd95f769cc8fa5baf8594
Aarav Navani <38411399+oofmeister27@users.noreply.github.com>
2024-06-22 18:16:38 -0700

    update llm code

--- backend/llm.py
+++ backend/llm.py
@@ -1,16 +1,19 @@
 import boto3
 import json
 
-bedrock = boto3.client(service_name="bedrock-runtime",region_name='us-east-1')
+bedrock = boto3.client(service_name="bedrock-runtime", region_name='us-east-1')
 
 modelId = "anthropic.claude-3-haiku-20240307-v1:0"
 
 accept = "application/json"
 contentType = "application/json"
-prompt = "What is life?"
+system_prompt = "You are a helpful assistant. Do not give the user the answer directly, but guide them towards finding the answer."
+user_prompt = "What is the derivative of 3x + 4?"
 
 response = bedrock.invoke_model(
     modelId=modelId,
+    contentType=contentType,
+    accept=accept,
     body=json.dumps(
         {
             "anthropic_version": "bedrock-2023-05-31",
@@ -18,24 +21,23 @@ response = bedrock.invoke_model(
             "messages": [
                 {
                     "role": "user",
-                    "content": [{"type": "text", "text": prompt}]
+                    "content": [{"type": "text", "text": system_prompt + " " + user_prompt}]
                 }
             ]
-
         }
     )
 )
 
-result = json.loads(response.get("body").read())
+result = json.loads(response.get("body").read().decode('utf-8'))
 input_tokens = result["usage"]["input_tokens"]
 output_tokens = result["usage"]["output_tokens"]
 output_list = result.get("content", [])
 
 print("Invocation details: ")
-print(f"- The input length is {input_tokens} ttokens" )
-print(f"- The output length is {output_tokens} ttokens" )
+print(f"- The input length is {input_tokens} tokens")
+print(f"- The output length is {output_tokens} tokens")
 
-print(f" - The model returned {len(output_list)} messages")
+print(f"- The model returned {len(output_list)} messages")
 
 for output in output_list:
     print(output["text"])
