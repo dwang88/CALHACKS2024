88a458dc5a0adec3db890c182652979508b878ba
Srikar Eranky <srikar.eranky@gmail.com>
2024-06-22 21:22:39 -0700

    user auth

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
-import pyrebase
+import firebase_admin
+from firebase_admin import credentials, auth
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 from dotenv import load_dotenv
@@ -9,20 +10,17 @@ app = Flask(__name__)
 
 load_dotenv()
 
-config = {
-  "apiKey": os.getenv("FIREBASE_API_KEY"),
-  "authDomain": "canvas-gpt.firebaseapp.com",
-  "projectId": "canvas-gpt",
-  "storageBucket": "canvas-gpt.appspot.com",
-  "messagingSenderId": "244500042332",
-  "appId": "1:244500042332:web:e96d6c28f4a2876a4ecc99",
-  "measurementId": "G-9J6KVLJXMG"
-}
+# config = {
+#   "apiKey": os.getenv("FIREBASE_API_KEY"),
+#   "authDomain": "canvas-gpt.firebaseapp.com",
+#   "projectId": "canvas-gpt",
+#   "storageBucket": "canvas-gpt.appspot.com",
+#   "messagingSenderId": "244500042332",
+#   "appId": "1:244500042332:web:e96d6c28f4a2876a4ecc99",
+#   "measurementId": "G-9J6KVLJXMG"
+# }
 
-firebase = pyrebase.initialize_app(config)
-auth = firebase.auth()
-
-uri = "mongodb+srv://anniesy2:Calhacks2024@teachers.6rnbrpj.mongodb.net/?retryWrites=true&w=majority&appName=Teachers"
+uri = os.getenv("DB_URL")
 
 client = MongoClient(uri, server_api=ServerApi('1'))
 
@@ -46,17 +44,11 @@ def add_student():
 @app.route('/add_teacher', methods=['POST'])
 def add_teacher():
     teacher_data = request.get_json()
-
-
-
-
-
     
 # @app.route('/get_teacher', methods=['GET'])
 # def get_teacher():
 #     try:
 #         teacher_data = collection.find_one({""})
-
 
 if __name__ == '__main__':
     app.run(debug=True)
